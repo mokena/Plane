e8cd20a4a5c310803d439f1f408845e6f5fa871a
wangrui2401 <wangrui2401@gmail.com>
2016-12-01 16:35:48 +0800

    init version

---
+++ Classes/StartScene.cpp
@@ -0,0 +1,58 @@
+#include "StartScene.h"
+
+USING_NS_CC;
+
+Scene* StartScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = StartScene::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool StartScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    // add "background" splash screen"
+    auto sprite = Sprite::create("bg.png");
+
+    // position the sprite on the center of the screen
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+
+	Label* startGameBtn = Label::createWithSystemFont("开始游戏", "Arial", 24);
+	startGameBtn->setString("开始游戏");
+	startGameBtn->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y - 30));
+	this->addChild(startGameBtn);
+    
+    return true;
+}
+
+
+void StartScene::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
